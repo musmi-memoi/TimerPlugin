82cfa4426a708bcd872f2672ce1c501f33978e9a
Rin-ad <yakusugi_rin@protonmail.com>
2023-11-03 20:04:27 +0900

    first commit

---
+++ src/main/java/jp/rinad/timer/Timer.java
@@ -0,0 +1,75 @@
+package jp.rinad.timer;
+
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.Bukkit;
+
+import java.awt.*;
+
+public final class Timer extends JavaPlugin {
+    private int countdownSeconds = 0;
+    private boolean isRunning = false;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getCommand("timer").setExecutor(new TimerCommand(this));
+    }
+
+    public void startTimer(int seconds) {
+        countdownSeconds = seconds;
+        isRunning = true;
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (countdownSeconds <= 0) {
+                    isRunning = false;
+                    cancel();
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+                        player.sendTitle("終了!!","時間になりました!!",10,70,20);
+                        player.playSound(player.getLocation(), "block.anvil.use",1.0F,1.0F);
+                    }
+                } else if (countdownSeconds <= 5) {
+
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+                        player.playSound(player.getLocation(),"entity.experience_orb.pickup",1.0F,1.0F);
+                        player.sendTitle(String.valueOf(countdownSeconds),"",10,70,20);
+                    }
+                } else if (countdownSeconds <= 10) {
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+                        player.sendTitle(String.valueOf(countdownSeconds),"",10,70,20);
+                    }
+                }
+
+                if (isRunning) {
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+                        String msg = formatTime(countdownSeconds);
+                        TextComponent component = new TextComponent();
+                        component.setText(msg);
+                        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, component);
+                    }
+                    countdownSeconds--;
+                }
+            }
+        }.runTaskTimer(this, 0, 20);
+    }
+
+    private String formatTime(int seconds) {
+        int minutes = seconds / 60;
+        int remainingSeconds = seconds % 60;
+        if (remainingSeconds < 10) {
+            // 秒が1桁の場合、0埋めで表示
+            return minutes + "分 0" + remainingSeconds + "秒";
+        } else {
+            return minutes + "分 " + remainingSeconds + "秒";
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/jp/rinad/timer/TimerCommand.java
@@ -0,0 +1,33 @@
+package jp.rinad.timer;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class TimerCommand implements CommandExecutor {
+    private final Timer plugin;
+    public TimerCommand(Timer plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender,Command command,String label,String[] args) {
+        if (args.length == 2 && args[0].equalsIgnoreCase("set")) {
+            try {
+                int seconds = Integer.parseInt(args[1]);
+                if (seconds > 0) {
+                    plugin.startTimer(seconds);
+                    sender.sendMessage("タイマーを開始");
+                } else {
+                    sender.sendMessage("秒数は正の整数で指定してください");
+                }
+
+            } catch (NumberFormatException e) {
+                sender.sendMessage("無効な秒数です");
+            }
+        } else {
+            sender.sendMessage("使用法: /timer set <second>");
+        }
+        return true;
+    }
+}
